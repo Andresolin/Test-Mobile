095d74e6ce339ec49305bd2cc9d366e6c0100ec1
Andre Permana Solin <permanasolin@gmail.com>
2024-03-17 20:44:21 +0700

    Menambahkan Navigation component

---
+++ app/src/main/java/org/d3if3002/testmobile/navigation/NavGraph.kt
@@ -0,0 +1,21 @@
+package org.d3if3002.testmobile.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import org.d3if3002.testmobile.ui.screen.MainScreen
+
+@Composable
+fun SetupNavGraph(navController: NavController = rememberNavController()) {
+    NavHost(
+        navController = navController as NavHostController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(route  = Screen.Home.route){
+            MainScreen()
+        }
+    }
+}
