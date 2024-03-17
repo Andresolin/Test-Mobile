4d093703b80ee8334fa56fb398c68173c7093812
Andre Permana Solin <permanasolin@gmail.com>
2024-03-17 21:38:43 +0700

    Membuat navigasi dengan tombol Up

--- app/src/main/java/org/d3if3002/testmobile/navigation/NavGraph.kt
+++ app/src/main/java/org/d3if3002/testmobile/navigation/NavGraph.kt
@@ -1,7 +1,6 @@
 package org.d3if3002.testmobile.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -12,7 +11,7 @@ import org.d3if3002.testmobile.ui.screen.MainScreen
 @Composable
 fun SetupNavGraph(navController: NavHostController = rememberNavController()) {
     NavHost(
-        navController = navController as NavHostController,
+        navController = navController,
         startDestination = Screen.Home.route
     ) {
         composable(route  = Screen.Home.route){
@@ -20,7 +19,7 @@ fun SetupNavGraph(navController: NavHostController = rememberNavController()) {
         }
 
         composable(route  = Screen.About.route){
-            AboutScreen()
+            AboutScreen(navController)
         }
     }
 }
