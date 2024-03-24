d9ff77b4e69a67f5a576de9e89806cd44f7871d7
Andre Permana Solin <permanasolin@gmail.com>
2024-03-25 01:43:57 +0700

    Menangani keadaan jika list kosong

--- app/src/main/java/org/d3if3002/testmobile/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3002/testmobile/ui/screen/MainScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -56,21 +57,39 @@ fun ScreenContent(modifier: Modifier) {
 
     val viewModel: MainViewModel = viewModel()
     val data = viewModel.data
+//    val data = emptyList<Catatan>()
 
-    LazyColumn(
-        modifier =modifier.fillMaxSize()
-    ){
-        items(data) {
-            ListItem(catatan = it)
-            Divider()
+    if (data.isEmpty()){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = stringResource(id = R.string.list_kosong))
+        }
+    } else {
+        LazyColumn(
+            modifier =modifier.fillMaxSize()
+        ){
+            items(data) {
+                ListItem(catatan = it)
+                Divider()
+            }
         }
     }
 }
 
+
+
+
 @Composable
 fun ListItem(catatan: Catatan) {
     Column(
-        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         Text(text = catatan.judul,
