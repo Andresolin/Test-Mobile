584b6416bb6ada687c05a24b213eccecf7cf3fe7
Andre Permana Solin <permanasolin@gmail.com>
2024-03-25 02:01:23 +0700

    .Menangani keadaan jika list kosong

--- app/src/main/java/org/d3if3002/testmobile/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3002/testmobile/ui/screen/MainScreen.kt
@@ -2,6 +2,9 @@ package org.d3if3002.testmobile.ui.screen
 
 
 import android.content.res.Configuration
+import android.service.autofill.OnClickAction
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,8 +12,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,6 +26,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -35,17 +43,29 @@ import org.d3if3002.testmobile.ui.theme.TestMobileTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+
     Scaffold(
         topBar = {
             TopAppBar(title = {
                 Text(text = stringResource(id = R.string.app_name))
             },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary
                 )
             )
-        }
+        },
+//        floatingActionButton = {
+//            FloatingActionButton(
+//                onClick = { Toast.makeText(context, R.string.belu)}
+//            ) {
+//                Icon(
+//                    imageVector = Icons.Filled.Add,
+//                    contentDescription = stringResource(id = R.string.tambah_catatan),
+//                    tint = MaterialTheme.colorScheme.primary
+//                )
+//            }
+//        }
     ) {padding ->
         ScreenContent(Modifier.padding(padding))
     }
@@ -57,6 +77,7 @@ fun ScreenContent(modifier: Modifier) {
 
     val viewModel: MainViewModel = viewModel()
     val data = viewModel.data
+    val context = LocalContext.current
 //    val data = emptyList<Catatan>()
 
     if (data.isEmpty()){
@@ -74,7 +95,10 @@ fun ScreenContent(modifier: Modifier) {
             modifier =modifier.fillMaxSize()
         ){
             items(data) {
-                ListItem(catatan = it)
+                ListItem(catatan = it){
+                    val pesan = context.getString(R.string.x_diklik, it.judul)
+                    Toast.makeText(context, pesan, Toast.LENGTH_SHORT).show()
+                }
                 Divider()
             }
         }
@@ -85,10 +109,11 @@ fun ScreenContent(modifier: Modifier) {
 
 
 @Composable
-fun ListItem(catatan: Catatan) {
+fun ListItem(catatan: Catatan, onClick: () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
+            .clickable { onClick }
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
